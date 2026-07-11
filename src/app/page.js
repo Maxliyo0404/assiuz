be050d0d0eb5d59f86c1ca004bf9f80a4c38feab
Mahliyo <gasandigital13@gmail.com>
2026-07-11 13:33:27 +0500

    assiuz star header

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ export default function Home() {
   return (
    <div className="home" >
      <div className="container">
-      <h1 className="text-4xl font-bold mt-10">Assalomu alaykum!</h1>
-      <p>Bu mening "Chakra Petch" shriftidagi birinchi sahifam.</p>
+     
     </div>
    </div>
   );
